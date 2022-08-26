3bd276e68919e154a1f6b8e2aa4eb3abc040b723
Alexander Gifford <alexandergifford77@gmail.com>
2022-08-26 15:22:07 -0700

    20220826-predelete

--- swd/src/components/landing.js
+++ swd/src/components/landing.js
@@ -13,18 +13,19 @@ export default function Landing(props){
  
     return(
         <div>
-             <div className="mb-20 overflow-x-hidden">
+              <div className="mb-20 overflow-x-hidden h-[100vh] ">
 
-                <div className="flex flex-row justify-between p-5 items-center">
-                <h1 className="font-main text-[20px] md:text-[30px]">Shoreline Web Developpment</h1>
-                <img alt="logo-black" onClick={props.toggle}  src={LogoB} className="w-[45px] h-[36px] cursor-pointer "></img>
-                </div>
-                <div>
-                    <ImageSlider slides={SliderData}/>
-                </div>
+                    <div className="flex flex-row justify-between p-5 items-center">
+                        <h1 className="font-main text-[20px] md:text-[30px]">Shoreline Web Developpment</h1>
+                        <img alt="logo-black" onClick={props.toggle}  src={LogoB} className="w-[45px] h-[36px] cursor-pointer "></img>
+                    </div>
+                        <div className="flex flex-col items-center justify-center h-[60vh]">
+                            <h1 className="text-[80px] mb-10">web-____</h1>
+                            <p className="text-[35px]">specialized blockchain websites for any project. </p>
+                        </div>
 
-                </div>
-
+               </div>
+ 
             <Services />
             <Work />
             <About />
